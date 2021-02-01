b0f909f25a0c34ed7c83f32604bd774c9f88b570
rbaul <4RFVbgt%>
2021-02-01 10:25:07 +0200

    Initial commit

---
+++ server/src/main/java/com/example/projectstructure/server/ProjectStructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectstructure.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStructureApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/projectstructure/server/ProjectStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectstructure.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
